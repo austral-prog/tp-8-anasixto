540b4566afc1f1bbd174e90bae3ec860273e35a0
anasixto <162721927+anasixto@users.noreply.github.com>
2024-05-24 11:39:02 -0300

    Update sets.py

--- sets.py
+++ sets.py
@@ -10,7 +10,9 @@ def clean_ingredients(dish_name, dish_ingredients):
 
 def check_drinks(drink_name, drink_ingredients):
 
-    
+    alcohols = {}
+
+    ingredients = set(drink_ingredientes)
 
     return
 
